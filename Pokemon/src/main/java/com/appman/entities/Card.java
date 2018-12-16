f3054bf5eef7ee5373d9ed1374d62debff9010aa
toon040 <toon040@hotmail.com>
2018-12-16 09:40:35 +0700

    insert spring boot app

---
+++ Pokemon/src/main/java/com/appman/entities/Card.java
@@ -0,0 +1,15 @@
+package com.appman.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Card {
+	
+	private String name;
+	private String type;
+	private int hp;
+	private int str;
+	private int weak;
+	private int favorite;
+	
+}
